b1a06233375c24789519e0ac3f0bc6cc1a45dedd
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-30 21:57:07 +0530

    Create 003_challenge(2).java

---
+++ 003_challenge(2).java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class starPattern{
+public static void main(String arg[]){
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
+    System.out.println("*****");
+}
+}
