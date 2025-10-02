04400fe60147a6600240d0b02e71d7ef9c336de4
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-10-02 10:35:47 +0530

    Create 006_explicit_conversion.java

---
+++ 006_explicit_conversion.java
@@ -0,0 +1,16 @@
+// explicit type conversion (casting);
+
+// Type conversion in Java
+
+import java.lang.*;
+
+public class main {
+    public static void main(String[] args){
+        float f = 323.33f;
+        int dn = (int) f;
+        
+        System.out.println(f);
+        System.out.println(dn);
+        
+    }
+}
