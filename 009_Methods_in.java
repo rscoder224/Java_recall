efeeb505d5225c6de201dcd14a701f0916e9021e
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-10-02 15:03:09 +0530

    Create 009_Methods_in.java

---
+++ 009_Methods_in.java
@@ -0,0 +1,27 @@
+// if else 
+import java.lang.*;
+import java.util.Scanner;
+
+
+public class methods{
+     
+static void addition(){
+        System.out.println("hello bro how are you");
+    }
+    
+    
+public static void main(String[] args){
+   
+    addition();
+    addition();
+    addition();
+    addition();
+    addition();
+    addition();
+    addition();
+    
+    
+    
+    }   
+   
+}
