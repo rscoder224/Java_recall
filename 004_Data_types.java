e62dc4b8b42cd339f8d11a581ea6d2dafcaa3e28
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-10-02 09:53:43 +0530

    Create 004_Data_types.java

---
+++ 004_Data_types.java
@@ -0,0 +1,27 @@
+// declaring dataypes in Java
+import java.lang.*;
+
+public class main{
+    public static void main(String[] args){
+        int i = 12;
+        short a = 234;
+        long b = 1888654;
+        float K = 353;
+        double S = 238443.53553535355353535;
+        char N = 's';
+        byte v = 1;
+        boolean ka = true;
+        String name = "Kallu Bhai";
+        
+        System.out.println(i);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(K);
+        System.out.println(S);
+        System.out.println(N);
+        System.out.println(v);
+        System.out.println(ka);
+        System.out.println(name);
+        
+    }
+}
