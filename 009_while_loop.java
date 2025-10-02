7e4a17c503b4cd1218ce96d81e72dc292e138b23
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-10-02 14:48:34 +0530

    Create 009_while_loop.java

---
+++ 009_while_loop.java
@@ -0,0 +1,19 @@
+// if else 
+import java.lang.*;
+import java.util.Scanner;
+
+public class whileLoop{
+    public static void main(String[] args){
+    
+    int i = 0;
+    int total = 0;
+     while (i <= 10) {
+         total = total + i;
+         i++;
+     }
+         System.out.println(total);
+        
+        
+    }   
+   
+}
