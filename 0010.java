5f6ca437a692de61ed6895aeb9ef76a65cf3f303
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-10-03 23:01:41 +0530

    Create 0010.java

---
+++ 0010.java
@@ -0,0 +1,14 @@
+
+import java.lang.*;
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args){
+        System.out.println("hello bro");
+    }
+public static String my(String name){
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter you age Here: ", name);
+}
+    
+}
