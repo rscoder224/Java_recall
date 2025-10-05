d977f3157d0e25255b995fe21d963ab1e1104540
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-10-05 21:21:41 +0530

    Create 012_Array_in.java

---
+++ 012_Array_in.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args){
+     int[] arr = new int[1];
+       arr[0] = 44;
+       int[] arr2 = {1,2,3,4,4,5};
+       
+       int i = 0;
+       while(i < arr2.length){
+           System.out.println(arr2[i]);
+           i++;
+       }
+    Scanner input = new Scanner(System.in);
+    
+ 
+    }
+}
